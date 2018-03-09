80cf4643e0fcc8e8fd091b630b7acf4ae82f67ee
Sergio Rabiela <sergiorabiela@gmail.com>
2018-03-08 23:02:27 -0500

    wip

--- _script/compose.rb
+++ _script/compose.rb
@@ -1,4 +1,20 @@
+require 'yaml'
+
 class Compose
-  def initialize(title, body)
+  attr_reader :title, :body
+
+  def initialize(title: nil, body:)
+    @title = title
+    @body = body
+  end
+
+  def call
+    post = {'layout' => 'post',
+            'date'   => Time.now.strftime('%F %R'),
+            'body'   => body
+    }
+    post['title'] = title if title
+    
+    YAML.dump(post)
   end
 end
